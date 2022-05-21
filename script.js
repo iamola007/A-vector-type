c498802c5d3d9ea681b45902b7f439082f0a5cd6
Ola Oladapo <olaoladapo007@gmail.com>
2022-05-21 17:16:53 +0100

    edit comments

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ class Vec {
     this.x = x;
     this.y = y;
   }
+
+  //adds two vectors together
   plus(other) {
     return new Vec(this.x + other.x, this.y + other.y);
   }
 
+  //finds the difference between two vectors
   minus(other) {
     return new Vec(this.x - other.x, this.y - other.y);
   }
 
+  //gets and calculates the distance between two vector points x and y
   get length() {
     return Math.sqrt(this.x * this.x, this.y * this.y);
   }
 }
 
 console.log(new Vec(2, 4).plus(new Vec(5, 7)));
-// → Vec{x: 3, y: 5}
+
 console.log(new Vec(3, 2).minus(new Vec(5, 3)));
-// → Vec{x: -1, y: -1}
+
 console.log(new Vec(4, 5).length);
-// → 5
